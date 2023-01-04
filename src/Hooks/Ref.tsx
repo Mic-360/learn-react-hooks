10ec37432174f7eefd6f00d6c96ca69a23abd421
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-01-05 04:58:19 +0530

    hooks: useRef

--- src/Hooks/Ref.tsx
+++ src/Hooks/Ref.tsx
@@ -1,15 +1,25 @@
-import { FC, useState } from 'react';
-import '../App.css';
+import { FC, useRef, useState } from 'react'
+import '../App.css'
 
 const Ref: FC = () => {
-    const [name, setName] = useState<string>('');
+  const [name, setName] = useState('')
+  const inputRef = useRef<HTMLInputElement | null>(null)
 
-    return (
-        <div id='Ref'>
-            <h1>useRef</h1>
-            <input value={name} onChange={e => setName(e.target.value)} />
-            <div>This is a {name} Hook</div>
-        </div>
-    );
+  function focus() {
+    // @ts-ignore
+    inputRef.current.focus()
+  }
+  return (
+    <div id='Ref'>
+      <h1>useRef</h1>
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <div>This is a {name} Hook</div>
+      <button onClick={focus}>FocusInput</button>
+    </div>
+  )
 }
-export default Ref;
+export default Ref
